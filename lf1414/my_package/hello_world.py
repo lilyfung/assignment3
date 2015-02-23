389ae376f9e2e51fe72e075de87d81e63b5316b0
lilyfung <lf1414@nyu.edu>
2015-02-22 20:55:26 -0500

    Adding Assignment 3 Files
    Adding Assignment 3 files to repository.

---
+++ lf1414/my_package/hello_world.py
@@ -0,0 +1,15 @@
+
+def hello_world():
+    '''
+Created on Feb 22, 2015
+
+@author: Lily Fung, lf1414@nyu.edu
+'''
+#
+# Python hello world
+#
+    if __name__ == "__main__":
+        import sys
+        print "Hello World!"
+
+hello_world()
